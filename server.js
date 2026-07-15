94c78c3d413bca796da19851f05fa4cf5dfd23e9
Javaworld22 <iheruomemichael@gmail.com>
2026-07-15 02:06:59 +0100

    commit 15.007.26

--- server.js
+++ server.js
@@ -7,8 +7,26 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'asa_sc_website.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.get('/overview', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'overview.html'));
+});
+
+app.get('/faq', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'faq.html'));
+});
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/advertisement', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'advertisement.html'));
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
